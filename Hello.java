9acdd3abd7578c643034b9ea76e03ee78193e6aa
Angkit Kharel <240428@softwarica.edu.np>
2025-04-30 08:47:30 +0545

    hello.java file added

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void greeting (String a) {
+        System.out.println(a);
+    }
+    public static void main(String [] args){
+        greeting("hello");
+    }
+}
